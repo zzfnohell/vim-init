a117831dcfbcad4c09f390304de914a2ebdf306b
Zongfeng Zhang <zhangzongfeng@xlfz.cn>
2018-07-03 14:13:24 +0800

    install

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 #! /bin/bash
 
-
-pushd ~
-
-pushd viminit
-git submodule update --init --recursive
-popd
-
-
-pushd .vim
-
-ln -s viminit/vim/pack
+pushd vimfiles
 
 pushd pack/plugins/start/YouCompleteMe
 python3 install.py --all
 popd
 
-popd
 
 popd
