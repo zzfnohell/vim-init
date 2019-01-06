26a264a8c19401d45685015e703782ffc9b2d697
Zongfeng Zhang <zzfnohell@gmail.com>
2019-01-07 00:05:53 +0800

    install.ps1

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ if [ ! -d ~/vimfiles ]; then
 fi
 
 pushd ~/vimfiles
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
 
