9443dadda15ccadc079b4461ccd1c5a7eea5c3fc
Zongfeng Zhang <zzfnohell@outlook.com>
2020-11-11 16:17:11 +0900

    install plug

--- install.sh
+++ install.sh
@@ -1,30 +1,3 @@
 #! /bin/bash
-if [ ! -d ~/vimfiles ]; then
-    mkdir -p ~/vimfiles
-fi
-
-pushd ~/vimfiles
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
-
-if [ ! -d pack/plugins/start/YouCompleteMe ]; then
-  mkdir -p pack/plugins/start
-
-  
-
-  #  https://github.com/Valloric/YouCompleteMe.git
-  pushd pack/plugins/start
-  
-  git clone https://github.com/Valloric/YouCompleteMe.git YouCompleteMe
-  
-  pushd YouCompleteMe
-  git submodule update --init --recursive
-  python3 install.py --all
-  popd 
-  
-  popd
-fi
-
-popd
